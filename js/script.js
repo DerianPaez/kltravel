b0512638c4337bd23c6ec78c582d731c2d0d6e7e
James <mendozajames02@hotmail.com>
2020-06-29 12:24:34 -0500

    Arreglos al responsive y Cambios a la segunda seccion

--- js/script.js
+++ js/script.js
@@ -10,54 +10,55 @@ function headerActive(){
     header.classList.add('header-is-active')
     header.style.color= "black"
     logo.src="images/Logo-black.png";
+    for(var i = 0; i < link.length; i++){
+        link[i].style.color = "black";
+    }
+    
 }
 
 function headerDesactive(){
     header.classList.remove('header-is-active')
     header.style.color= "white"
     logo.src="images/Logo-white.png";
-    if (!menu.classList.contains('menu-is-active')){
-        for(var i = 0; i < link.length; i++){
-            link[i].style.color = "black";
-        }
+    for(var i = 0; i < link.length; i++){
+        link[i].style.color = "white";
     }
 }
 
-mediaQuery.addListener(validation)
-validation(mediaQuery)
-function validation(evento) {
+document.addEventListener('scroll', dinamycMenu)
+        function dinamycMenu() {
+            if (window.pageYOffset >= 30) {
+                headerActive()
+            }  else {
+                headerDesactive()
+                
+            }
+        }
+
+// mediaQuery.addListener(validation)
+// validation(mediaQuery)
+// function validation(evento) {
     
-    if (evento.matches){
-        console.log("true")
-        document.addEventListener('scroll', dinamycMenu)
-        function dinamycMenu() {
-            if (window.pageYOffset >= 30) {
-                headerActive()
-            }  else {
-                headerDesactive()
-                for(var i = 0; i < link.length; i++)
-                {
-                    link[i].style.color = "white";
-                }
-            }
-        }
+//     if (evento.matches){
+//         console.log("true")
+//         document.addEventListener('scroll', dinamycMenu)
+//         function dinamycMenu() {
+//             if (window.pageYOffset >= 30) {
+//                 headerActive()
+//             }  else {
+//                 headerDesactive()
+//                 for(var i = 0; i < link.length; i++)
+//                 {
+//                     link[i].style.color = "white";
+//                 }
+//             }
+//         }
 
-    } else {
-        console.log("false")
-        document.addEventListener('scroll', dinamycMenu)
-        function dinamycMenu() {
-            if (window.pageYOffset >= 30) {
-                headerActive()
-            }  else {
-                headerDesactive()
-                for(var i = 0; i < link.length; i++)
-                {
-                    link[i].style.color = "black";
-                }
-            }
-        }
-    }
-}
+//     } else {
+//         console.log("false")
+        
+//     }
+// }
 
 
 
