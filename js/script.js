59eadb8e1f9fc35feae799eae30e576c580d4d36
James <mendozajames02@hotmail.com>
2020-07-03 18:44:47 -0500

    Arreglo del responsive de la barra de navegacion

--- js/script.js
+++ js/script.js
@@ -4,60 +4,22 @@ const logo = document.getElementById('logo')
 const link = document.getElementsByClassName('link')
 const menu = document.getElementById('menu-mobile')
 
-const mediaQuery = window.matchMedia('screen and (min-width: 768px)')
-
-function headerActive(){
-    header.classList.add('header-is-active')
-    header.style.color= "black"
-    logo.src="images/Logo-black.png";
-    for(var i = 0; i < link.length; i++){
-        link[i].style.color = "black";
-    }
-}
-
-function headerDesactive(){
-    header.classList.remove('header-is-active')
-    header.style.color= "white"
-    logo.src="images/Logo-white.png";
-    for(var i = 0; i < link.length; i++){
-        link[i].style.color = "white";
-    }
-}
-
 document.addEventListener('scroll', dinamycMenu)
-        function dinamycMenu() {
-            if (window.pageYOffset >= 30) {
-                headerActive()
-            }  else {
-                headerDesactive()
-                
-            }
-        }
 
-// mediaQuery.addListener(validation)
-// validation(mediaQuery)
-// function validation(evento) {
-    
-//     if (evento.matches){
-//         console.log("true")
-//         document.addEventListener('scroll', dinamycMenu)
-//         function dinamycMenu() {
-//             if (window.pageYOffset >= 30) {
-//                 headerActive()
-//             }  else {
-//                 headerDesactive()
-//                 for(var i = 0; i < link.length; i++)
-//                 {
-//                     link[i].style.color = "white";
-//                 }
-//             }
-//         }
+function dinamycMenu(){
+    if (window.pageYOffset >= 30) {
 
-//     } else {
-//         console.log("false")
+        header.classList.add('header-is-active')
+        header.style.color= "black"
+        logo.src="images/Logo-black.png";
         
-//     }
-// }
+    } else {
+        header.classList.remove('header-is-active')
+        header.style.color= "white"
+        logo.src="images/Logo-white.png";
+
+    }
+} 
 
 
 // Menu Hamburger
